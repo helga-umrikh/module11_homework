0d30f3ef561415461b241b6f723ddfc86fbee4a9
helga-umrikh <zetaumr@gmail.com>
2023-03-18 03:51:58 +0300

    feat: add check for empty input

--- task_2/task_2.js
+++ task_2/task_2.js
@@ -4,5 +4,10 @@ export const greetingFn = () => {
     let name =  prompt('What is your name?');
     let lastName = prompt('What is you last name?');
     
+    if(name === '' || lastName === '' ) {
+        return name = alert('Empty input, please, restart and try again');
+      };
+
+
     alert(`Hi, ${name} ${lastName}`);
 };
