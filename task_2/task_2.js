0534f3273a9f632590bd6687a6a900ffd8662cc7
helga-umrikh <zetaumr@gmail.com>
2023-03-18 03:34:42 +0300

    feat: add export to task_2.js

--- task_2/task_2.js
+++ task_2/task_2.js
@@ -1,6 +1,6 @@
 //Напишите программу, которая спрашивает у пользователя его имя и фамилию с помощью prompt и отвечает: Привет, {имя} {фамилия}! с помощью alert.
 
-const greetingFn = () => {
+export const greetingFn = () => {
     let name =  prompt('What is your name?');
     let lastName = prompt('What is you last name?');
     
